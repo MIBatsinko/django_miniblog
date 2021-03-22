ca7a159532feebfe2af1d25b3c70f2f490d4c777
Maksym Batsinko <batsinko.m@gmail.com>
2021-03-22 20:41:58 +0200

    Fix

--- miniblog/miniblog/settings.py
+++ miniblog/miniblog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%+y4n#yfu%p@)6mxwc$w)i8@3%!k7ym*vcv8j#(#6abv*v6*@1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
